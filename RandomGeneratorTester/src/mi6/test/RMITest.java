e6194ac4e5879f5374d9f3683b1bd20e3c0bac66
Lucas Churchill <lucas@agtlucas.com>
2014-05-26 08:32:36 -0300

    Update test

--- RandomGeneratorTester/src/mi6/test/RMITest.java
+++ RandomGeneratorTester/src/mi6/test/RMITest.java
@@ -36,15 +36,15 @@ public class RMITest {
             props.setProperty("java.naming.factory.url.pkgs",
                     "com.sun.enterprise.naming");
 
-//            props.setProperty("java.naming.factory.state",
-//                    "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            props.setProperty("java.naming.factory.state",
+                    "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
 
-//            // optional.  Defaults to localhost.  Only needed if web server is running 
-//            // on a different host than the appserver    
-//            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-//
-//            // optional.  Defaults to 3700.  Only needed if target orb port is not 3700.
-//            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+            // optional.  Defaults to localhost.  Only needed if web server is running 
+            // on a different host than the appserver    
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+
+            // optional.  Defaults to 3700.  Only needed if target orb port is not 3700.
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
             
             ctx = new InitialContext(props);
             RandomGeneratorRemote rgr = (RandomGeneratorRemote) ctx.lookup(JNDI_NAME);
